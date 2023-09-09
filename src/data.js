7ebdbf0cdaae58d0da7bc64bd88207287fba2034
DaYuan Lin <dayuanlin@oursong.com>
2023-09-09 23:05:50 +0800

    go to

--- src/data.js
+++ src/data.js
@@ -15,7 +15,7 @@ export { HeroDark as Dark };
  **************************************************************
   Add your GitHub username (string - "YourUsername") below.
 */
-export const githubUsername = "Your GitHub username here";
+export const githubUsername = "see2-club";
 
 // Blog link icon - https://icon-sets.iconify.design/
 export const Blog = <Icon icon="ph:link-bold" />;
